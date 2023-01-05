24a289ef7bfc3b200fbb82070ddf50465e4fe8eb
Fekhreddine23 <fekhredine1990@gmail.com>
2023-01-05 15:16:10 +0100

    exos finis

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,12 @@ package java8.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.junit.Test;
@@ -18,13 +22,23 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
+		
 		Date date = null;
+	    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		
+
+	    String date1 = "12/02/2017";
+	    
+	    date = simpleDateFormat.parse(date1);
 
 		// TODO transformer la date en Instant
+		Instant now = date.toInstant();
+		
+		ZoneId currentZone = ZoneId.systemDefault();
 
 		// TODO transformer la date en LocalDate
 
-		LocalDate result = null;
+		LocalDate result = LocalDate.ofInstant(now, currentZone);
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
